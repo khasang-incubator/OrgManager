74e9992c3006f7cad6eb1202425a2a10d295d83e
Andrii Bohomolov <andrey.bogomoloff@gmail.com>
2016-04-06 21:38:12 +0300

    Add field field to Sapper class

--- src/main/java/io/khasang/sapper/Sapper.java
+++ src/main/java/io/khasang/sapper/Sapper.java
@@ -1,16 +1,20 @@
 package io.khasang.sapper;
 
 public class Sapper {
+	
+	public static final int SIZE = 10;
 
+	private int[][] field;
+	
+	public Sapper() {
+		this.field = new int[SIZE][SIZE];
+	}
 
+    public int[][] getField(){
+        return this.field;
+    }
+    
     public static void main(String[] args) {
 
     }
-
-    public int[][] getField(){
-
-        return null;
-    }
-
-
 }

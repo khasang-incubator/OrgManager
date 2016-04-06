3776b5f81761ade07c02e7b26510e4ee63806533
Andrii Bohomolov <andrey.bogomoloff@gmail.com>
2016-04-06 22:35:01 +0300

    Implement Sapper#getField() method

--- src/test/java/io/khasang/sapper/TestSapper.java
+++ src/test/java/io/khasang/sapper/TestSapper.java
@@ -6,18 +6,18 @@ import org.junit.Test;
 public class TestSapper {
 	@Test
 	public void testSapperInitialization() {
-		Sapper sapper =  new Sapper();
+		Sapper sapper = new Sapper();
 		Assert.assertNotNull(sapper);
 		int[][] field = sapper.getField();
-		Assert.assertEquals(field.length,10);
-		Assert.assertEquals(field[0].length,10);
+		Assert.assertEquals(field.length, Sapper.FIELD_SIZE);
+		Assert.assertEquals(field[0].length, Sapper.FIELD_SIZE);
 		int bombsCount = 0;
 		for (int row = 0; row < field.length; row++) {
 			for (int col = 0; col < field.length; col++) {
-				int cellValue = sapper.getValue(row,col);
-				if (cellValue == 1) {
+				int cellValue = sapper.getValue(row, col);
+				if (cellValue == Sapper.VALUE_BOMB) {
 					bombsCount++;
-				} else if (cellValue != 0) {
+				} else if (cellValue != Sapper.VALUE_EMPTY) {
 					Assert.fail("Unexpected cell value: " + cellValue);
 				}
 			}

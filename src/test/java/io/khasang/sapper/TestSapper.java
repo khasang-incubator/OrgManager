61dbc7cb7ad90679e18cd54b24a1b995d6319a29
Andrii Bohomolov <andrey.bogomoloff@gmail.com>
2016-04-06 21:07:06 +0300

    Create new test for Sapper

---
+++ src/test/java/io/khasang/sapper/TestSapper.java
@@ -0,0 +1,13 @@
+package io.khasang.sapper;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestSapper {
+	
+	@Test
+	public void test() {
+		Assert.assertNotNull(new Sapper());
+	}
+
+}

37eeaaeed34a05aceecc2b746668e3ab34123c44
avpgenium <avpgenium@yandex.ru>
2016-04-22 00:11:13 +0500

    partly java config

--- src/main/java/io/khasang/orgmanager/config/AppContext.java
+++ src/main/java/io/khasang/orgmanager/config/AppContext.java
@@ -1,13 +1,23 @@
 package io.khasang.orgmanager.config;
 
+import io.khasang.orgmanager.model.DataSelect;
 import io.khasang.orgmanager.model.Hello;
 import io.khasang.orgmanager.model.SecureAccess;
 import io.khasang.orgmanager.model.SuperSecureAccess;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 @Configuration
+@PropertySource("classpath:config.properties")
 public class AppContext {
+    @Autowired
+    Environment environment;
+
     @Bean
     public Hello hello(){
         return new Hello();
@@ -21,5 +31,27 @@ public class AppContext {
     @Bean
     public SuperSecureAccess superSecureAccess(){
         return new SuperSecureAccess();
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate(){
+        JdbcTemplate jdbcTemplate = new JdbcTemplate();
+        jdbcTemplate.setDataSource(dataSource());
+        return jdbcTemplate;
+    }
+
+    @Bean
+    public DriverManagerDataSource dataSource(){
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(environment.getProperty("jdbc.postgresql.driverClass"));
+        dataSource.setUrl(environment.getProperty("jdbc.postgresql.url"));
+        dataSource.setUsername(environment.getProperty("jdbc.postgresql.username"));
+        dataSource.setPassword(environment.getProperty("jdbc.postgresql.password"));
+        return dataSource;
+    }
+
+    @Bean
+    public DataSelect dataSelect(){
+        return new DataSelect(jdbcTemplate());
     }
 }

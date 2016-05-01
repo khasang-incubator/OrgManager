6ef0e5fc581f69f4583924f3638bd05051ec23f5
Radiofisik <mail@radiofisik.ru>
2016-05-02 00:43:47 +0200

    backup added

--- src/main/java/io/khasang/orgmanager/model/Backup.java
+++ src/main/java/io/khasang/orgmanager/model/Backup.java
@@ -1,6 +1,7 @@
 package io.khasang.orgmanager.model;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.support.rowset.SqlRowSet;
@@ -16,6 +17,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+@PropertySource("classpath:config.properties")
 @Component
 public class Backup {
     @Autowired

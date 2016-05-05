6d8efe59c51cea70de0cb73bcf2e24fe4589e0dd
Radiofisik <mail@radiofisik.ru>
2016-05-05 23:53:07 +0200

    view resolvers conflict solved

--- src/main/java/io/khasang/orgmanager/config/application/WebConfig.java
+++ src/main/java/io/khasang/orgmanager/config/application/WebConfig.java
@@ -8,36 +8,42 @@ import org.springframework.web.servlet.config.annotation.DefaultServletHandlerCo
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
 import org.springframework.web.servlet.view.tiles3.TilesConfigurer;
 import org.springframework.web.servlet.view.tiles3.TilesViewResolver;
 
 
 @Configuration
 @EnableWebMvc
-@ComponentScan({"io.khasang.orgmanager.config" })
+@ComponentScan({"io.khasang.orgmanager.config"})
 public class WebConfig extends WebMvcConfigurerAdapter {
 
-    /*   @Bean
-    public ViewResolver viewResolver() {
-        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
-        viewResolver.setViewClass(JstlView.class);
-        viewResolver.setPrefix("/WEB-INF/views/");
-        viewResolver.setSuffix(".jsp");
+    @Bean
+    public ViewResolver tilesViewResolver() {
+        TilesViewResolver viewResolver=new TilesViewResolver();
+        viewResolver.setOrder(0);
         return viewResolver;
-    }*/
+    }
 
-   @Bean
-    public TilesConfigurer tilesConfigurer(){
-        TilesConfigurer tiles=new TilesConfigurer();
-        tiles.setDefinitions(new String[]{"/WEB-INF/layout/tiles.xml","/WEB-INF/layout/admintiles.xml"});
+    @Bean
+    public TilesConfigurer tilesConfigurer() {
+        TilesConfigurer tiles = new TilesConfigurer();
+        tiles.setDefinitions(new String[]{"/WEB-INF/layout/tiles.xml", "/WEB-INF/layout/admintiles.xml"});
         tiles.setCheckRefresh(true);
         return tiles;
     }
 
     @Bean
-    public ViewResolver viewResolver(){
-        return new TilesViewResolver();
+    public ViewResolver viewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setViewClass(JstlView.class);
+        viewResolver.setPrefix("/WEB-INF/views/");
+        viewResolver.setSuffix(".jsp");
+        viewResolver.setOrder(1);
+        return viewResolver;
     }
+
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {

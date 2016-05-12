05c704663f11a29290b5d6d320427b2bae60e88c
radiofisik <atolkov@uecard39.ru>
2016-05-12 13:44:00 +0200

    draft user rest

--- src/main/java/io/khasang/orgmanager/service/rest/UserService.java
+++ src/main/java/io/khasang/orgmanager/service/rest/UserService.java
@@ -5,18 +5,50 @@ import io.khasang.orgmanager.dao.IUserDao;
 import io.khasang.orgmanager.model.Entities.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 
 
 @RestController
-@RequestMapping("/rest/user")
+@CrossOrigin(origins = "*")
 public class UserService {
     @Autowired
     IUserDao userDao;
 
-    @RequestMapping(method = RequestMethod.GET)
-    public User getUser(@RequestParam(value = "id") Integer id){
+    @RequestMapping( value = "/rest/user/{id}", method = RequestMethod.GET)
+    public User getUser(@PathVariable("id") Integer id){
        return userDao.get(id);
+    }
+
+    @RequestMapping( value = "/rest/users", method = RequestMethod.GET)
+    public List<User> getUsers() {
+        return userDao.getAll();
+    }
+
+    @RequestMapping(value = "/rest/user", method = RequestMethod.POST)
+    public ResponseEntity<String> createEmployee(@RequestBody User user)
+    {
+        System.out.println(user);
+        userDao.save(user);
+        return new ResponseEntity(HttpStatus.CREATED);
+    }
+
+    @RequestMapping(value = "/rest/user/{id}", method = RequestMethod.PUT)
+    public ResponseEntity<User> updateEmployee(@PathVariable("id") int id, @RequestBody User user)
+    {
+        System.out.println(id);
+        System.out.println(user);
+        userDao.save(user);
+        return new ResponseEntity<User>(user, HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/rest/user/{id}", method = RequestMethod.DELETE)
+    public ResponseEntity<String> updateEmployee(@PathVariable("id") int id)
+    {
+        System.out.println(id);
+        userDao.delete(userDao.get(id));
+        return new ResponseEntity(HttpStatus.OK);
     }
 }
